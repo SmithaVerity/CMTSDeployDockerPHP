97edc4a245b75ea978bdfbeaae952cb374125f97
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-11 13:44:56 +0530

    Update deployDocker.sh

--- deployDocker.sh
+++ deployDocker.sh
@@ -2,4 +2,3 @@ sudo apt update && sudo apt install docker.io --yes
 sudo docker pull aspendigital/codeigniter
 sudo docker run --name collegemgmtapp -p 80:80  -d aspendigital/codeigniter:latest
 sudo docker exec -it collegemgmtapp /bin/bash
-apt update
