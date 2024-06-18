539dad8a343a21e41e4d4aa176b3bac6de5853ea
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-18 18:00:45 +0530

    Update deployDockerandApp.sh

--- deployDockerandApp.sh
+++ deployDockerandApp.sh
@@ -1,4 +1,4 @@
 sudo apt update && sudo apt install docker.io --yes
 sudo docker pull aspendigital/codeigniter
-sudo docker run --name collegemgmtapp -p 80:80  -d aspendigital/codeigniter:latest
+sudo docker run --name collegemgmtapp -p 80:80  -d shinsenter/codeigniter4:latest
 sudo docker exec -it collegemgmtapp /bin/bash
