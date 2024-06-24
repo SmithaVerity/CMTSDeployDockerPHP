cb61f1481558f01ec40f5276487bbf7415375f5f
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-24 23:45:36 +0530

    Update config.php

--- CMTSCollegeMgmt/application/config/config.php
+++ CMTSCollegeMgmt/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://43.205.116.25/CollegeMgmt/';
+$config['base_url'] = 'https://54.252.170.91/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------
