1876b7e08e50a836ed233be6890c75473c81322c
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-24 23:45:08 +0530

    Update deployDockerandApp.sh

--- deployDockerandApp.sh
+++ deployDockerandApp.sh
@@ -1,4 +1,4 @@
 sudo apt update && sudo apt install docker.io --yes
 
-sudo docker run --name collegemgmtapp -p 80:80 -v ./CMTSCollegeMgmt:/var/www/html -d shinsenter/codeigniter4:latest
+sudo docker run --name collegemgmtapp -v ./CMTSCollegeMgmt:/var/www/html/CMTSCollegeMgmt -p 80:80 -d shinsenter/codeigniter4:latest
 sudo docker exec -it collegemgmtapp /bin/bash
