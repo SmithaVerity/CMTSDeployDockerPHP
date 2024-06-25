76004d34471e1cb2442d6ee08fd4e330cf967855
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-25 12:57:48 +0530

    Update deployDockerandApp.sh

--- deployDockerandApp.sh
+++ deployDockerandApp.sh
@@ -1,5 +1,6 @@
 sudo apt update && sudo apt install docker.io --yes
 
-sudo docker run --name collegemgmtapp -p 80:80  -d bitnami/codeigniter:latest
-sudo docker exec -it collegemgmtapp /bin/bash
+# sudo docker run --name collegemgmtapp -p 80:80  -d bitnami/codeigniter:latest
+sudo docker run --name collegemgmtapp -v /CMTSCollegeMgmt:/app bitnami/codeigniter:latest
+# sudo docker exec -it collegemgmtapp /bin/bash
 
